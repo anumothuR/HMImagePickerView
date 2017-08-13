dc3ef5718dc806e62636f5aaa8ee42d762bc037c
anumothu <hmspl@Nandha-2.local>
2017-08-13 11:43:00 +0530

    Soruce code update

---
+++ Example/Pods/Target Support Files/HMImagePickerView/HMImagePickerView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HMImagePickerView : NSObject
+@end
+@implementation PodsDummy_HMImagePickerView
+@end
---
+++ Example/Pods/Target Support Files/Pods-HMImagePickerView_Example/Pods-HMImagePickerView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HMImagePickerView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HMImagePickerView_Example
+@end
